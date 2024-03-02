35b8f4d4d10077120f6de7fc6cd39685d94670be
Rennan Alves <rennan.alves@blip.ai>
2024-03-01 23:02:04 -0300

    Criacao do projeto

---
+++ SistemaHospedagem/Program.cs
@@ -0,0 +1,48 @@
+﻿using SistemaHospedagem.Models;
+using SistemaHospedagem.Services;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+try
+{
+    Console.WriteLine("=========== HOTEL MARICA ===========");
+    Console.Write("Digite o tipo da suíte: ");
+    string tipoSuite = Console.ReadLine();
+    Console.Write("Digite a capacidade de hospedes da suíte: ");
+    int capacidade = int.Parse(Console.ReadLine());
+    Console.Write("Digite o valor da diária da suíte: ");
+    decimal diariaSuite = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a capacidade de da suíte: ");
+    Suite suite = new Suite(tipoSuite, capacidade, diariaSuite);
+
+    Console.Clear();
+
+    Console.Write("Digite a quantidade de hospedes: ");
+    int n = int.Parse(Console.ReadLine());
+    List<Pessoa> hospedes = new List<Pessoa>();
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.Clear();
+        Console.Write($"Digite o nome do {i + 1} hospede: ");
+        string nome = Console.ReadLine();
+        Console.Write($"Digite o sobrenome do {i + 1} hospede: ");
+        string sobrenome = Console.ReadLine();
+        Pessoa pessoa = new Pessoa(nome, sobrenome);
+        hospedes.Add(pessoa);
+    }
+
+    Reserva reserva = new Reserva(diasReservados: 10);
+    reserva.CadastrarSuite(suite);
+    reserva.CadastrarHospedes(hospedes);
+
+    Console.WriteLine();
+    Console.WriteLine($"Suíte: {reserva.Suite}");
+    Console.WriteLine($"Quantidade de Hospedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor da hospedagem: {reserva.CalcularValorDiario()}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message );
+}
---
+++ SistemaHospedagem/Services/Reserva.cs
@@ -0,0 +1,52 @@
+﻿using SistemaHospedagem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHospedagem.Services
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade < hospedes.Count)
+            {
+                throw new Exception("Ultrapassou a quantidade máxima de hospedes para essa suíte.");
+            }
+            else
+            {
+                Hospedes = hospedes;
+            }       
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiario()
+        {
+            if (DiasReservados >= 10)
+            {
+                return Suite.ValorDiaria * DiasReservados * 90 / 100;
+            }
+            return Suite.ValorDiaria * DiasReservados;
+        }
+    }
+}
